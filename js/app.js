f19cd35d211f1f4f4ac7607e334ff97114980274
Ivan Neto <ivan.cr.neto@gmail.com>
2014-05-31 01:30:35 -0300

    tabs using controller

--- js/app.js
+++ js/app.js
@@ -32,4 +32,15 @@
     }
   ];
 
+  app.controller('PanelController', function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab) {
+      return this.tab == checkTab;
+    }
+  });
+
 })();
